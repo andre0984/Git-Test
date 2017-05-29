7282dd2071859b8d717ee8ad77e91a437e07772b
Suritno <suritno@transkom.local>
2017-05-29 16:35:00 +0700

    make changes 123

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry=input("are you hungry ?")
-if hungry=="Yes" and hungry=="Y" and (suritno xxxxx):
+if hungry=="Yes" and hungry=="Y" and (suritno 1234):
  print("eat Samosa")
  print("eat Pizza")
  print("eat Burger")
